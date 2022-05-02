18e4531e1fc4fb040af763e871faab36adfda778
KillReal63 <104447337+KillReal63@users.noreply.github.com>
2022-05-02 11:34:20 +0400

    Kir
    add button

--- main.js
+++ main.js
@@ -2,20 +2,32 @@ import './style.css';
 import './variables.css';
 
 const container = document.getElementById('container');
+const leftButton = document.getElementById('left-button');
+const rightButton = document.getElementById('right-button');
+rightButton.addEventListener('click', () => handleButtonClick());
+
+const handleButtonClick = side => {
+  const id = container.getAttribute('data-month-id');
+  if (id <= 11) {
+    renderMonth(year[Number(id)]);
+  } else {
+    renderMonth(year[0]);
+  }
+};
 
 const year = [
-  { name: 'January', length: 31 },
-  { name: 'February', length: 28 },
-  { name: 'March', length: 31 },
-  { name: 'April', length: 30 },
-  { name: 'May', length: 31 },
-  { name: 'June', length: 30 },
-  { name: 'July', length: 31 },
-  { name: 'August', length: 31 },
-  { name: 'September', length: 30 },
-  { name: 'October', length: 31 },
-  { name: 'November', length: 30 },
-  { name: 'December', length: 31 },
+  { name: 'January', length: 31, id: 1 },
+  { name: 'February', length: 28, id: 2 },
+  { name: 'March', length: 31, id: 3 },
+  { name: 'April', length: 30, id: 4 },
+  { name: 'May', length: 31, id: 5 },
+  { name: 'June', length: 30, id: 6 },
+  { name: 'July', length: 31, id: 7 },
+  { name: 'August', length: 31, id: 8 },
+  { name: 'September', length: 30, id: 9 },
+  { name: 'October', length: 31, id: 10 },
+  { name: 'November', length: 30, id: 11 },
+  { name: 'December', length: 31, id: 12 },
 ];
 
 const date = year.map(({ name, length }) => {
@@ -26,8 +38,14 @@ const date = year.map(({ name, length }) => {
   return { name, month };
 });
 
-const getMonth = month => {
-  const currentMonth = date.filter(({ name }) => month === name);
+const renderMonth = ({ name: monthName, length, id: monthId }) => {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+  container.setAttribute('data-month-id', monthId);
+  const currentMonthName = document.getElementById('month-name');
+  currentMonthName.innerText = monthName;
+  const currentMonth = date.filter(({ name }) => monthName === name);
   currentMonth[0].month.map(({ id }) => {
     const days = document.createElement('div');
     days.innerText = id;
@@ -36,4 +54,4 @@ const getMonth = month => {
   });
 };
 
-getMonth('January');
+renderMonth(year[0]);
